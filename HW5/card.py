de90689f00d9c5a3bbea44655fd507751e99a1fc
IlliaLazaruk <lazaruk252002@gmail.com>
2023-03-06 21:09:31 +0200

    create Suits(Enum) for better code view

--- HW5/card.py
+++ HW5/card.py
@@ -1,27 +1,34 @@
 import itertools
 import random
 import time
+from enum import Enum
 
 
+class Suits(Enum):
+    spades = '\u2660'
+    clubs = '\u2663'
+    hearts = '\u2665'
+    diamonds = '\u2666'
+
+    def __format__(self, format_spec):
+        return f"{self.value}"
 class Cards:
     """Class for magic game with cards"""
+    Suits = Suits
+
+
     ranks = list(range(6, 10 + 1)) + list('JQKA')
-    suits = {
-        '\u2660',  # spades
-        '\u2663',  # clubs
-        '\u2665',  # hearts
-        '\u2666'}  # diamonds
 
     @classmethod
     def cards_deck(cls):
         """Return full cards deck"""
-        deck = list(itertools.product(cls.ranks, cls.suits))
+        deck = list(itertools.product(cls.ranks, cls.Suits))
         return deck
 
     @classmethod
     def random_card(cls):
         """Return one random card"""
-        deck = list(itertools.product(cls.ranks, cls.suits))
+        deck = list(itertools.product(cls.ranks, cls.Suits))
         card = random.choice(deck)
         return f"{card[0]} {card[1]}"
 
@@ -43,22 +50,22 @@ class Cards:
 
         Cards.wait_for_divination()
 
-        if '\u2660' in one_card:  # if card is ♠
+        if Cards.Suits.spades.value in one_card:  # if card is ♠
             if '6' in one_card:
                 print("Стовідсотково ні! Також тебе чекають великі неприємності!!! Бережи себе.")
             else:
                 print("Нажаль ні(")
-        elif '\u2663' in one_card:  # if card is ♣
+        elif Cards.Suits.clubs.value in one_card:  # if card is ♣
             print(
                 "Швидше за все ні(. Але якщо ти будеш дуже старатись, то можливо й зможеш повернути "
                 "фортуну у свій бік.")
-        elif '\u2665' in one_card:  # if card is ♥
+        elif Cards.Suits.hearts.value in one_card:  # if card is ♥
             if 'Q' in one_card:
                 print("Тебе чекає велика удача. Однозначно так)))")
             else:
                 print("Дуже вірогідно що це збудеться.")
         else:  # if card is ♦
-            print("Можливо це й щбудеться! Але це не точно.")
+            print("Можливо це й збудеться! Але це не точно.")
 
 
 if __name__ == '__main__':
--- HW5/fortune.py
+++ HW5/fortune.py
@@ -1,18 +1,17 @@
 from card import Cards as divination
 
+# consts
+YES = "так"
+NO = "ні"
 
-def start_divination():
-    print("Привіт, я ворожка. Чи хочеш ти дізнатись відповідь на своє питання?")
+print("Привіт, я ворожка.")
+while True:
+    print("Чи хочеш ти дізнатись відповідь на своє питання?")
     user_choice = input("Введи 'Так' чи 'Ні': ")
-
-    while True:
-        if user_choice == "Так":
-            print("Галя, ти не виділа мої карти? Тут хочуть щоб я поворожила. О, знайшла!")
-            return divination.cards_divination()
-        elif user_choice == "Ні":
-            break
-        else:
-            user_choice = input("Введи 'Так' чи 'Ні': ")
-
-
-start_divination()
+    if user_choice.lower().strip() == YES:
+        print("Галя, ти не виділа мої карти? Тут хочуть щоб я поворожила. О, знайшла!")
+        divination.cards_divination()
+    elif user_choice.lower().strip() == NO:
+        break
+    else:
+        print("Ти ввів щось неправильне")
